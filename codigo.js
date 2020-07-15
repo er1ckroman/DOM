cd2ef5e4a6587d8055692ac2282a00d71aef11ec
Erick Alonso <erick.alonso@pslgroup.com>
2020-07-15 14:45:46 -0500

    my first commit

---
+++ codigo.js
@@ -0,0 +1,6 @@
+var ruta = window.location;
+        console.log(ruta);
+        console.log(navigator);
+        console.log(window);
+        console.log(console);
+        document.write("Estás en: " + ruta);
